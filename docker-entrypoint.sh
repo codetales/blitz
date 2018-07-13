6ac5c43d5ab204c4c36d3bf9ea9a6279eacd140e
Julian Fahrer <julian@fahrer.net>
2018-07-12 22:02:54 -0700

    Add a reset command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -28,6 +28,17 @@ case "$COMMAND" in
     RUN="${UNSION_COMMAND} -repeat watch \"${SRC_PATH}\" \"${DEST_PATH}\""
     ;;
 
+  reset)
+    while true; do
+      read -p "A reset will delete all data in ${UNISON_DATA} and ${DEST_PATH}. Do you want to proceed? " yn
+      case $yn in
+        [Yy]* ) rm -rfv "${UNISON_DATA}/*" "${DEST_PATH}/*"; exit;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+      esac
+    done
+    ;;
+
   *)
     exec $COMMAND "$@"
     ;;
