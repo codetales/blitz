9b9c71c9203c34c627dfa827d261a770a4cd3c20
Julian Fahrer <julian@fahrer.net>
2019-06-21 20:18:58 -0700

    Integrate monit and supervisord

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,54 @@
-FROM alpine:3.7
+FROM alpine:3.9
 
-RUN apk add --no-cache bash unison su-exec
+RUN apk add --no-cache \
+        bash \
+        gettext \
+        monit \
+        su-exec \
+        supervisor
 
-ENV UNISON_DATA=/unison_data
-ENV UNISONLOCALHOSTNAME=syncer
-ENV SRC_PATH=/src DEST_PATH=/dest
-ENV USERID=0 USERNAME=syncer
-ENV DEFAULT_OPTS -batch -auto
-ENV UNISON_OPTS=""
+ARG UNISON_VERSION=2.51.2
+ENV UNISON_VERSION=$UNISON_VERSION
+RUN apk add --no-cache build-base curl ocaml \
+      && curl -L https://github.com/bcpierce00/unison/archive/v$UNISON_VERSION.tar.gz | tar zxv -C /tmp \
+      && cd /tmp/unison-${UNISON_VERSION} \
+      && sed -i -e 's/GLIBC_SUPPORT_INOTIFY 0/GLIBC_SUPPORT_INOTIFY 1/' src/fsmonitor/linux/inotify_stubs.c \
+      && make UISTYLE=text NATIVE=true STATIC=true \
+      && apk del build-base curl ocaml \
+      && cp src/unison src/unison-fsmonitor /usr/local/bin \
+      && rm -rf /var/cache/apk/* \
+      && rm -rf /tmp/unison-${UNISON_VERSION}
+
+
+ARG UNISON_DATA=/unison_data
+ARG HOST_DATA_PATH=/host
+ARG CONTAINER_DATA_PATH=/container
+ENV \
+  UNISONLOCALHOSTNAME=syncer \
+  UNISON_DATA=/unison_data \
+  UNISON_DEFAULT_OPTS="-batch -auto" \
+  UNISON_OPTS="" \
+  CONTAINER_DATA_PATH=$CONTAINER_DATA_PATH \
+  HOST_DATA_PATH=$HOST_DATA_PATH \
+  SYNC_USERID=0 \
+  SYNC_USER=syncer \
+  WATCHGUARD_CHECK_INTERVAL=1 \
+  WATCHGUARD_ENABLED=true \
+  WATCHGUARD_HIGH_CPU_THRESHOLD=60 \
+  WATCHGUARD_HIGH_CPU_CYCLES=5
+
+RUN mkdir -p \
+      /docker-entrypoint.d \
+      /etc/supervisor.conf.d \
+      $UNISON_DATA \
+      $CONTAINER_DATA_PATH \
+      $HOST_DATA_PATH
 
 COPY docker-entrypoint.sh /
+COPY monitrc.template supervisord.conf /etc/
+
+VOLUME $UNISON_DATA
+VOLUME $CONTAINER_DATA_PATH
+
 ENTRYPOINT ["/docker-entrypoint.sh"]
 CMD ["run"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
 set -e
+
+###
+# Setting up the environment
+###
+if [ "$SYNC_UID" -ne 0 ]
+then
+  export SYNC_USER_HOME=/home/${SYNC_USER}
+  deluser ${SYNC_USER} --remove-home || true
+  adduser -h ${SYNC_USER_HOME} -s /bin/bash -u $SYNC_UID -D ${SYNC_USER}
+  chown -R ${SYNC_USER}:${SYNC_USER} ${SYNC_USER_HOME} "${UNISON_DATA}" "${CONTAINER_DATA_PATH}"
+  [ -L /home/${SYNC_USER}/.unison ] || ln -s "${UNISON_DATA}" ${SYNC_USER_HOME}/.unison
+  export SYNC_EXECUTOR="su-exec ${SYNC_USER}"
+else
+  [ -L /root/.unison ] || ln -sf "${UNISON_DATA}" /root/.unison
+  export SYNC_USER_HOME=/root
+  export SYNC_USER=root
+  export SYNC_EXECUTOR="exec"
+fi
+
+###
+# Starting ssync
+###
+
+UNSION_COMMAND="unison ${UNISON_DEFAULT_OPTS} ${UNISON_OPTS}"
+
 COMMAND=${1-run}
 shift
 
-[ -d "${UNISON_DATA}" ] || mkdir "${UNISON_DATA}"
-
-if [ "$USERID" -ne 0 ]
-then
-  deluser ${USERNAME} --remove-home || true
-  adduser -h /home/${USERNAME} -s /bin/bash -u $USERID -D ${USERNAME}
-  chown -R ${USERNAME}:${USERNAME} /home/${USERNAME} "${UNISON_DATA}" "${DEST_PATH}"
-  [ -L /home/${USERNAME}/.unison ] || ln -s "${UNISON_DATA}" /home/${USERNAME}/.unison
-  EXECUTOR="su-exec ${USERNAME}"
-else
-  [ -L /root/.unison ] || ln -sf "${UNISON_DATA}" /root/.unison
-  EXECUTOR="exec"
-fi
-
-UNSION_COMMAND="${EXECUTOR} unison ${DEFAULT_OPTS} ${UNISON_OPTS}"
-
 case "$COMMAND" in
   run)
-    RUN="${UNSION_COMMAND} \"${SRC_PATH}\" \"${DEST_PATH}\""
+    echo ${SYNC_EXECUTOR} ${UNSION_COMMAND} "${HOST_DATA_PATH}" "${CONTAINER_DATA_PATH}"
+    eval ${SYNC_EXECUTOR} ${UNSION_COMMAND} "${HOST_DATA_PATH}" "${CONTAINER_DATA_PATH}"
     ;;
 
   watch)
-    RUN="${UNSION_COMMAND} -repeat watch \"${SRC_PATH}\" \"${DEST_PATH}\""
+    envsubst /etc/monitrc.template > /etc/monitrc
+    # sed ... > monitrc
+    export UNISON_START_COMMAND="${UNSION_COMMAND} -repeat watch \"${HOST_DATA_PATH}\" \"${CONTAINER_DATA_PATH}\""
+    exec supervisord
     ;;
 
   reset)
     while true; do
-      read -p "A reset will delete all data in ${UNISON_DATA} and ${DEST_PATH}. Do you want to proceed? " yn
+      read -p "A reset will delete all data in ${UNISON_DATA} and ${CONTAINER_DATA_PATH}. Do you want to proceed? " yn
       case $yn in
-        [Yy]* ) rm -rf "${UNISON_DATA}/"* "${DEST_PATH}"/*; exit;;
+        [Yy]* ) rm -rf "${UNISON_DATA}/"* "${CONTAINER_DATA_PATH}"/*; exit;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
       esac
@@ -43,6 +56,3 @@ case "$COMMAND" in
     exec $COMMAND "$@"
     ;;
 esac
-
-echo $RUN
-eval $RUN
